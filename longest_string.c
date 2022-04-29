58fcf8f98420e7dd5433a633d051f11de9f64344
kshitij-jadhav <kshitij.jadhav@cctech.co.in>
2022-04-29 11:37:05 +0530

    corrected the message for null input.

--- longest_string.c
+++ longest_string.c
@@ -2,19 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc,char *argv[]) {
-	int longestStringIndex;
-	if(argc<=1) {
-		puts("enmpty string");
-	}
-	else {
-		longestStringIndex=1;
-		for(int i=2;i<argc;i++) {
-			if(strlen(argv[i])>strlen(argv[longestStringIndex])) {
-				longestStringIndex=i;
+int main(int argc, char *argv[]) {
+	int longestStringIndex = 1;
+	if (argc <= 1) {
+		puts("empty string");
+	}else {
+		int longestStringLength,currentStringLength;
+		for (int i = 2; i < argc; i++) {
+			longestStringLength = strlen(argv[longestStringIndex]);
+			currentStringLength = strlen(argv[i]);
+			if (currentStringLength > longestStringLength) {
+				longestStringIndex = i;
 			}
 		}
 	}
-	printf("%s\n",argv[longestStringIndex]);
+	puts(argv[longestStringIndex]);
 	return EXIT_SUCCESS;
 }
